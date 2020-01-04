9b72d58445b8ce6ba3daa8d675a61d2667079299
vipul550 <hivipulhere203@gmail.com>
2020-01-04 18:23:16 +0530

    initial commit
    initial commit

---
+++ precache-manifest.0456eb85f7766e989657f789fa8b29e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd39f199e45b13f01928c2bd340353bd",
+    "url": "/yoloj-master/index.html"
+  },
+  {
+    "revision": "7d2df0328d9604926c7c",
+    "url": "/yoloj-master/static/css/main.1d5628ed.chunk.css"
+  },
+  {
+    "revision": "a6c26d1e19dc0f408bfd",
+    "url": "/yoloj-master/static/js/2.43ab10da.chunk.js"
+  },
+  {
+    "revision": "7d2df0328d9604926c7c",
+    "url": "/yoloj-master/static/js/main.8b2284fb.chunk.js"
+  },
+  {
+    "revision": "cdf53a9c587aa3c2598f",
+    "url": "/yoloj-master/static/js/runtime~main.d4fcf57a.js"
+  },
+  {
+    "revision": "c0f1e4a4fdfb8048c72e86aadb2a247d",
+    "url": "/yoloj-master/static/media/Roboto-Bold.c0f1e4a4.woff2"
+  },
+  {
+    "revision": "eed9aab5449cc9c8430d7d258108f602",
+    "url": "/yoloj-master/static/media/Roboto-Bold.eed9aab5.woff"
+  },
+  {
+    "revision": "3c37aa69cd77e6a53a067170fa8fe2e9",
+    "url": "/yoloj-master/static/media/Roboto-Light.3c37aa69.woff2"
+  },
+  {
+    "revision": "ea36cd9a0e9eee97012a67b8a4570d7b",
+    "url": "/yoloj-master/static/media/Roboto-Light.ea36cd9a.woff"
+  },
+  {
+    "revision": "1561b424aaef2f704bbd89155b3ce514",
+    "url": "/yoloj-master/static/media/Roboto-Medium.1561b424.woff2"
+  },
+  {
+    "revision": "cf4d60bc0b1d4b2314085919a00e1724",
+    "url": "/yoloj-master/static/media/Roboto-Medium.cf4d60bc.woff"
+  },
+  {
+    "revision": "3cf6adf61054c328b1b0ddcd8f9ce24d",
+    "url": "/yoloj-master/static/media/Roboto-Regular.3cf6adf6.woff"
+  },
+  {
+    "revision": "5136cbe62a63604402f2fedb97f246f8",
+    "url": "/yoloj-master/static/media/Roboto-Regular.5136cbe6.woff2"
+  },
+  {
+    "revision": "1f35e6a11d27d2e10d28946d42332dc5",
+    "url": "/yoloj-master/static/media/Roboto-Thin.1f35e6a1.woff2"
+  },
+  {
+    "revision": "44b78f142603eb69f593ed4002ed7a4a",
+    "url": "/yoloj-master/static/media/Roboto-Thin.44b78f14.woff"
+  },
+  {
+    "revision": "7aa93e505582dd40013e86e8f06a4dbf",
+    "url": "/yoloj-master/static/media/down-arrow.7aa93e50.jpg"
+  }
+]);
